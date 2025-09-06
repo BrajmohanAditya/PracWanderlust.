455f385423d7f6979d518eb27f15a112406cdcad
Mohan Kumar <cckumarsingh38ar@gmail.com>
2025-09-06 11:16:10 +0530

    change

--- init/index.js
+++ init/index.js
@@ -1,36 +1,38 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-
 // Establishing connection to Data base ---> (Step-3)
-const MONGO_URL = "mongodb://127.0.0.1:27017/PracWonderlust"; 
-main().then(()=>{
+// const MONGO_URL = "mongodb://127.0.0.1:27017/PracWonderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
     console.log("connected to DB");
-})
-.catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-});
-async function main(){   
-   await mongoose.connect(MONGO_URL);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 //***
-
 
 // Inserting Data into Data base  ---> (Step-4)
-const initDB = async()=>{
+const initDB = async () => {
   await Listing.deleteMany({});
   //step: 19, aim: implementing authorization, work: adding owner to every listing
-  initData.data = initData.data.map((obj) => ({ ...obj, owner: "68b69321d380f858486c551a" }));
- //---
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68bbc771a24561acf523b6f8",
+  }));
+  //---
   await Listing.insertMany(initData.data); // line no: 2 dekho
   console.log("data has initialized");
-}
+};
 initDB();
 //***
-
-
-
 
 /*
 
